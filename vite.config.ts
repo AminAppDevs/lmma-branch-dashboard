a2e9ca460832105f574ce5fd724c93d11e171684
AminAppDevs <90038071+AminAppDevs@users.noreply.github.com>
2023-06-18 00:36:36 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 5173, // you can replace this port with any port
+    port: 5173,
+    hmr: {
+      host: "31.220.80.92",
+      port: 443,
+      protocol: "wss",
+    },
   },
 });

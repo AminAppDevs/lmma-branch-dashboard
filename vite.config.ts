a166debb2d4ab5bab6d35bd909c867303a7835e5
Mohamed Alawad <amin.app.devs@gmail.com>
2023-06-19 02:03:59 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,12 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+    strictPort: true,
     host: true, // needed for the Docker Container port mapping to work
     port: 5173, // you can replace this port with any port
+    hmr: {
+      clientPort: 5173,
+      port: 5174,
+    },
   },
 });

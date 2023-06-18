c4b59cb22e2ab53864df5188f6261ee7bb5b14c5
AminAppDevs <90038071+AminAppDevs@users.noreply.github.com>
2023-06-18 21:35:34 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
-    https: true,
     hmr: {
       host: "lmma-branch-dash.captin.nadwatalmaerifa.com",
       port: 5173,
